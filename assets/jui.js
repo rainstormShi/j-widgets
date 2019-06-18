1b9b2c07cf5d4ee882173784d67c49b053ab3948
rainstormShi <905412162@qq.com>
2019-06-18 11:43:49 +0800

    小修改

--- assets/jui.js
+++ assets/jui.js
@@ -41,24 +41,32 @@
     }
     // 身份证验证
     ,isIdentityCard:function(cardId){
-      var aCity={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",  
+      var aCity = {11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",  
       21:"辽宁",22:"吉林",23:"黑龙江",31:"上海",32:"江苏",33:"浙江",  
       34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北",  
       43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川"  
       ,52:"贵州",53:"云南",54:"西藏",61:"陕西",62:"甘肃",63:"青海",  
       64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外"} 
-      var iSum=0 ;  
-      var info="" ;  
-      if(!/^\d{17}(\d|x)$/i.test(cardId)) return "请输入正确的身份证号码2";
-      cardId=cardId.replace(/x$/i,"a");  
-      if(aCity[parseInt(cardId.substr(0,2))]==null) return "请输入正确的身份证号码1";
-      sBirthday=cardId.substr(6,4)+"-"+Number(cardId.substr(10,2))+"-"+Number(cardId.substr(12,2));  
-      var d=new Date(sBirthday.replace(/-/g,"/")) ;  
-      if(sBirthday!=(d.getFullYear()+"-"+ (d.getMonth()+1) + "-" + d.getDate()))return "请输入正确的身份证号码";
+      var iSum = 0 ;
+      var info = "" ;
+      if(!/^\d{17}(\d|x)$/i.test(cardId)){
+        return "身份证长度或格式错误";
+      }
+      cardId=cardId.replace(/x$/i,"a");
+      if(aCity[parseInt(cardId.substr(0,2))] == null){
+        return "城市地区不正确";
+      }
+      sBirthday = cardId.substr(6,4)+"-"+Number(cardId.substr(10,2))+"-"+Number(cardId.substr(12,2));
+      var d = new Date(sBirthday.replace(/-/g,"/")) ;  
+      if(sBirthday != (d.getFullYear()+"-"+ (d.getMonth()+1) + "-" + d.getDate())){
+        return "出生日期不正确";
+      }
       // 以下验证为准确验证 一般情况 不考虑(如测试时)
-      // for(var i = 17;i>=0;i --) iSum += (Math.pow(2,i) % 11) * parseInt(cardId.charAt(17 - i),11) ;  
-      // if(iSum%11!=1) return alert("你输入的身份证号非法");  
-      return true;//aCity[parseInt(cardId.substr(0,2))]+","+sBirthday+","+(cardId.substr(16,1)%2?"男":"女")
+      // for(var i = 17;i> = 0;i --) iSum += (Math.pow(2,i) % 11) * parseInt(cardId.charAt(17 - i),11);
+      // if(iSum%11 != 1){
+      //   return "你输入的身份证号非法";
+      // }
+      return true;
     }
     // 表单验证
     ,verifyData:function(){
